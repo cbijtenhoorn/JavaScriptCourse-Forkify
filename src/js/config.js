61a3516733e8a13db1bee0d43248cce1e40d5e5e
ENK\stagiaire <stagiaire@enk.nl>
2023-05-15 10:38:15 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a0a83f45-fa49-4077-a66f-3b0dc6f84cfb';
+export const MODAL_CLOSE_SEC = 2.5;
